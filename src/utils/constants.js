bf659acdfb0a1e28f2e6bc15c9c258c1c68cc9b9
pandu-jakkula <pandu.jakkula@bilvantis.io>
2024-08-07 16:30:03 +0530

    ui changes for the login page

---
+++ src/utils/constants.js
@@ -0,0 +1,9 @@
+export const loginTextSamples = {
+  LOGIN: 'Login to Bilvantis',
+  MICROSOFT_IMAGE: 'https://cdn.kekastatic.net/login/v/M178_2024.06.15.1/images/logos/microsoft.svg',
+  MICROSOFT_ALT: 'Microsoft Logo',
+  MICROSOFT_TEXT: 'Microsoft',
+  BILVANTIS_LOGO_IMAGE: require('../assets/images/BilvantisWhiteLogo.png'),
+  LOGIN_WITH: 'Login with',
+  BILVANTIS_LOGO_ALT: 'Bilvnatis'
+};
